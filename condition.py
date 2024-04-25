7656439179be8eae1ba31807a899fb4f6cf3b68b
rahul suryawanshi <kambirereshma88@gmail.com>
2024-04-25 16:57:58 +0530

    first commit

---
+++ condition.py
@@ -0,0 +1,9 @@
+a=1
+b=1
+if a==b:
+    print(True)
+else:
+    print(False)
+
+    
+    
--- fourth.py
+++ fourth.py
@@ -34,5 +34,73 @@ b = (1,2,3,3,3,3,3,5,3,5)
 print(b)
 print(type(b))
 
+#map
+"""
+Dictionary
+"""
+
+a = {'name':'reshma','age':45}
+print(a)
+print(type(a))
+
+
+#set
+
+a = {'rohit','abc','rahul',11,'xyz',11,'xyz'}
+print(a)
+
+
+#boolean
+b = True
+print(type(b))
+
+
+#membership operator
+
+a='reshma'
+b=[1,2,3,4,5,'abc','xyz']
+print('i' in a)
+print( 11 not in b)
+
+
+a = 12
+b = 13
+print(a is not b)
+
+a = 12
+b = 12
+print(a is b)
+
+
+print("first")
+print("second")
+print("third\nforth")
+
+print(1,end="   ")
+print(2,end="   ")
+print(3,end="   ")
+print(4,end="   ")
+print(5,)
+
+
+
+"**********"
+
+print("*  "*10)
+print("hello  "*5)
+print("*"*1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
